0d191b5293ecf4cf29b83a7dbf28c7e3adf2a58d
briansun <imbriansun@gmail.com>
2020-09-17 21:49:22 +0800

    Comment Constructor of Employee Class

--- Ch4/src/EmployeeTest.java
+++ Ch4/src/EmployeeTest.java
@@ -35,6 +35,9 @@ class Employee{
 
     // Constructor
     public Employee(String n, double s, int year, int month, int day){
+//      String name = "11111";
+//      在构造器里的局部变量仅仅只能由构造器可见，并且最好不要与对象的实例字段重名，除非对对象的字段使用this.来修改其值
+//      否则这些构造器的局部变量会遮盖同名的实例字段导致错误
         this.name = n;
         this.salary = s;
         this.hireDay = LocalDate.of(year, month, day);
